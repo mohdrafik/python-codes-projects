811499cd290728f45fd36eb7d3ae260275ff37e4
mohdrafik <rafikiitbhu@gmail.com>
2024-04-05 17:06:10 +0200

    commented the set_axis for the ampdf and phasedf --> beacuse already set the axis like ampdf.set_axis(['Piezo','Amplitude'], axis ='columns') in the python file converdfSetAxisgetNumpyArray.py and this return the result in form of res[0],res[1],res[2]..  -- can be seen in the Process_plot_data.ipynb.   and finally this reverseArrayofAvgWindow.py return the list of reversed list named as list_avg

--- reverseArrayOfAvgWindow.py
+++ reverseArrayOfAvgWindow.py
@@ -1,16 +1,15 @@
-"""here in arguments we will always try to give the  dataframe like ampdf and phasedf 
+"""
+here in arguments we will always try to give the  dataframe like ampdf and phasedf 
 ampdf (as a dataframe given to the function) --> ampdf   = pd.read_excel(comp_datapathAmp)  
 phasedf (as a dataframe given to the function) --> phasedf = pd.read_excel(comp_datapathPhase)
-
- """
-
+"""
 import numpy as np
 import pandas as pd
 # import matplotlib.pyplot as plt
 
 def reverseArrayofAvgValuesWndsize(ampdf,phasedf,avg_window):
-    ampdf= ampdf.set_axis(['Piezo','Amplitude'], axis ='columns')
-    phasedf= phasedf.set_axis(['Piezo','Phase'], axis ='columns')
+    # ampdf = ampdf.set_axis(['Piezo','Amplitude'], axis ='columns')
+    # phasedf = phasedf.set_axis(['Piezo','Phase'], axis ='columns')
     # print("\n",phasedf.head(),"\n",phasedf.shape,"\n")
     #  < ----------- Above changed the column name(set index) from x,y to Piezo, Amplitude and Phase 
     ampdfAmplitudeColumn = np.array(ampdf['Amplitude'])    # convert to numpy array
@@ -26,14 +25,15 @@ def reverseArrayofAvgValuesWndsize(ampdf,phasedf,avg_window):
     count_window = 0
     list_avg = []
     count_listavgEntry = 0
+    
 
     for val in y:
-        count_window =count_window + 1
+        count_window = count_window + 1
         list_avg_windowSize.append(val)
     #     print(list_avg_windowSize,end=' ') 
-        if len(list_avg_windowSize)== avg_window:
+        if len(list_avg_windowSize) == avg_window:
     #         print(list_avg_windowSize)
-            count_listavgEntry +=1
+            count_listavgEntry += 1
             avg_ofwindowsizeElements = sum(list_avg_windowSize)/avg_window
     #         print(avg_ofwindowsizeElements,end =' ,')
             list_avg.append(avg_ofwindowsizeElements)
