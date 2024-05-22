c495f759580f28600a04ae611363f3e1f3494055
mohdrafik <rafikiitbhu@gmail.com>
2024-05-22 18:06:40 +0200

    added a optional argument want_plot = None leave as it is don'create the plot irrelevant jsut for testing if you want to see plot than want_plot = 1 :

--- xlsfileprocess/final_index_calculation.py
+++ xlsfileprocess/final_index_calculation.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-def findActualInflexion(inflexion_After_avg,list_avg,data_endamp,avg_window,ampdfAmplitudeColumnarr,ampdfPiezoColumnarr):
+def findActualInflexion(inflexion_After_avg,list_avg,data_endamp,avg_window,ampdfAmplitudeColumnarr,ampdfPiezoColumnarr,want_plot=None):
 
     inflexion_index_afterAverage   = inflexion_After_avg
     y_scatter_indexforavg = list_avg[inflexion_index_afterAverage]
@@ -9,7 +9,6 @@ def findActualInflexion(inflexion_After_avg,list_avg,data_endamp,avg_window,ampd
     plt.grid()
     plt.ylim([7,9.2])
     plt.show()
-
 
 
     # below I will calculate the actual inflexion point with the original index of the ampdf.
@@ -26,9 +25,13 @@ def findActualInflexion(inflexion_After_avg,list_avg,data_endamp,avg_window,ampd
     y_actual = ampdfAmplitudeColumnarr[final_Actual_index]
     x_scatter = ampdfPiezoColumnarr[final_Actual_index] 
     # plt.figure
-    plt.plot(ampdfPiezoColumnarr,ampdfAmplitudeColumnarr,'-r',alpha=0.2)
-    plt.scatter(x_scatter,y_actual,marker ='<',s=150,alpha=0.8,label='Data(nm)')
-    plt.title(" it is final ")
-    plt.grid()
-    plt.show()
+    if want_plot == None:
+        pass
+    else:
+        plt.plot(ampdfPiezoColumnarr,ampdfAmplitudeColumnarr,'-r',alpha=0.2)
+        plt.scatter(x_scatter,y_actual,marker ='<',s=150,alpha=0.8,label='Data(nm)')
+        plt.title(" it is final ")
+        plt.grid()
+        plt.show()
+
     return final_Actual_index
